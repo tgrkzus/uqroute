cf1f0198f3131265357efc0aa045284e5a6eef1f
Tom Richardson <tgrkzus@gmail.com>
2016-08-26 16:11:18 +0100

    Correctly fetches json data, stripping it of unnecessary info and caching it locally

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os.path, requests, json
+import os.path, requests, json, sys
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -7,31 +7,46 @@ class Class(object):
         self.name = name
         self.buildingNumber = buildingNumber
 
+def strip_data(data):
+    finalDict = dict()
+    for i in data:
+        element = data[i][0]
+        bNum = element["metadata"]["buildingNum"]
+        if bNum == None or element["campusID"] != 0:
+            continue
+        else:
+            finalDict[bNum] = {
+            "title": element["title"],
+            "latitude": element["latitude"],
+            "longitude": element["longitude"]
+            }
+    return finalDict
+
 def fetch_data():
-    if not os.path.isfile("/locations.cache"):
+    if not os.path.isfile("location.cache"):
         locationFile = open("location.cache", "w")
         r = requests.get("http://uqmaps.app.uq.edu.au/json/locations")
-        # strip unnecessary info
-
-        locationFile.write(r.text)
+        data = strip_data(r.json())
+        locationFile.write(json.dumps(data))
         locationFile.close()
 
     # Read from file now
 
     locationFile = open("location.cache", "r")
+    #print(locationFile.read(), file=sys.stderr)
     data = json.loads(locationFile.read())
+    locationFile.close()
     return data
     
 def request_location_info(latitude, longitude):
     data = fetch_data()
-    pass
+    return data
 
 def display_map():
     class1 = Class("Class 1 - Forgen Smith", 1)
     class2 = Class("Class 2 - Hawken", 50)
 
-    return request_location_info(0, 0)
-    pass
+    return request_location_info(0, 0)["58A"]["title"]
 
 def get_path_info():
     return '''
