8ffec1b79a2211e584ea551c6a741fc5d570d09a
tgrkzus@gmail.com <tgrkzus@gmail.com>
2016-09-02 15:35:32 +0100

    removed locate control library

--- static/js/gps_path.js
+++ static/js/gps_path.js
@@ -9,7 +9,6 @@ function createMap() {
     // Decode input:
     target = L.latLng(-27.4980000, 153.0131141);
     var map = L.map('map').setView([-27.4989042, 153.0131141], 13);
-    L.control.locate().addTo(map);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18,
